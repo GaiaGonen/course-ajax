9cb71bc8b264be660b17173365429c3d853a436f
GG <gaiagonen@gmail.com>
2018-09-24 23:05:56 +0300

    fix: Authorize for unsplash API

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -14,11 +14,17 @@
 const searchedForText = 'hippos';
 const unsplashRequest = new XMLHttpRequest();
 
-unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+unsplashRequest.open('GET', `https://api.unsplash.com/photos/?client_id=241fddf0e44aa4e7757f77196d6df2c68aab3ea166cc073e881dca79159f2192&page=1&query=${searchedForText}`);
 unsplashRequest.onload = addImage;
-unsplashRequest.setRequestHeader('Authorization', 'Client-ID <your-client-id>');
+// unsplashRequest.setRequestHeader('Authorization', 'Client-ID 241fddf0e44aa4e7757f77196d6df2c68aab3ea166cc073e881dca79159f2192');
 unsplashRequest.send();
 
 function addImage(){
-
+  debugger;
 }
+
+function addArticles () {}
+const articleRequest = new XMLHttpRequest();
+articleRequest.onload = addArticles;
+articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=26d6148aac33436f8db38c158b715fce`);
+articleRequest.send();
