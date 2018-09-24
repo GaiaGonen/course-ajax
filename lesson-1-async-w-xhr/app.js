085397e246049a1bc6318c35640f6dca7c8fe522
GG <gaiagonen@gmail.com>
2018-09-24 22:42:36 +0300

    feat: Request the searched text from unsplash

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -10,3 +10,15 @@
         searchedForText = searchField.value;
     });
 })();
+
+const searchedForText = 'hippos';
+const unsplashRequest = new XMLHttpRequest();
+
+unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+unsplashRequest.onload = addImage;
+unsplashRequest.setRequestHeader('Authorization', 'Client-ID <your-client-id>');
+unsplashRequest.send();
+
+function addImage(){
+
+}
